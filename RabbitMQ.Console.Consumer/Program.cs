ce0cf874eed9840cf316a0b1d13cd7a32abaf7c9
CanCumaYaman <65663050+CanCumaYaman@users.noreply.github.com>
2021-04-18 02:21:56 +0300

    Add project files.

---
+++ RabbitMQ.Console.Consumer/Program.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Common.Models;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Console.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName="localhost",
+                UserName="cancuma",
+                Password="cancuma."
+            };
+            var conneciton = factory.CreateConnection();
+            var channel = conneciton.CreateModel();
+            channel.QueueDeclare(
+                queue:"TakeOrderQueue",
+                durable:false,
+                exclusive:false,
+                autoDelete:false,
+                arguments:null
+                );
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, mq) =>
+              {
+                  var data = Encoding.UTF8.GetString(mq.Body.ToArray());
+                  var consumeData = JsonConvert.DeserializeObject<TakeOrderRequest>(data);
+                  System.Console.WriteLine(String.Format("{0} ID user has bought the product named {1}", consumeData.CustomerId, consumeData.ProductName));
+
+              };
+            channel.BasicConsume(
+                queue:"TakeOrderQueue",
+                autoAck:true,
+                consumer:consumer
+                );
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ RabbitMQ.Web.Api/Controllers/ShoppingController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShoppingController : ControllerBase
+    {
+        [HttpPost("TakeOrder")]
+        public IActionResult TakeOrder(TakeOrderRequest request)
+        {
+            try
+            {
+                var factory = new ConnectionFactory()
+                {
+                    HostName = "localhost",
+                    UserName = "cancuma",
+                    Password = "cancuma."
+                };
+                var connection = factory.CreateConnection();
+                var channel = connection.CreateModel();
+                channel.QueueDeclare(
+                    queue: "TakeOrderQueue",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                    );
+                for(int i = 1; i <= 100; i++)
+                {
+                    request.CustomerId = i;
+                    var data = JsonConvert.SerializeObject(request);
+                    var byteData = Encoding.UTF8.GetBytes(data);
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: "TakeOrderQueue",
+                        basicProperties: null,
+                        body: byteData
+                        );
+                }
+                return Ok("Success");
+                }
+                catch(Exception e)
+            {
+                return Ok(e.Message);
+            
+            }
+        }
+    }
+}
